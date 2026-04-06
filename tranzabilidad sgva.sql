14b1d7da61fcfb9e6cb8a5efa813fe6f74c91694
ING-JUAN0813 <juand081318@gmail.com>
2026-04-06 14:14:27 +0000

    sql tranzabilidad sgva

---
+++ tranzabilidad sgva.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE tanzabilidad_sgva;
+USE tanzabilidad_sgva;
+
+CREATE table Tanzabilidad (
+id_Tranzabilidad int primary key not null auto_increment,
+Codigo_Empresa varchar(100) not null,
+telefono varchar(100) not null,
+Correo varchar(100) not null,
+Estado varchar(100) not null,
+Encargado varchar(100) not null,
+Fecha_inicio datetime not null,
+fecha_fin  datetime not null
+);
+CREATE TABLE Empresa(
+id_Empresa int primary key  not null auto_increment,
+Nombre_Empresa_Empresa varchar(100) not null
+);
