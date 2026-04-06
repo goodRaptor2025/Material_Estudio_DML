999a0b5a4f2272791e72255b98f4cf6444a0d5f5
ING-JUAN0813 <juand081318@gmail.com>
2026-04-06 16:00:23 +0000

    tranzabilidad

--- tranzabilidad sgva.sql
+++ tranzabilidad sgva.sql
@@ -1,17 +1,54 @@
 CREATE DATABASE tranzabilidad_sgva;
-USE tanzabilidad_sgva;
+USE tranzabilidad_sgva;
 
-CREATE table Tranzabilidad (
-id_Tranzabilidad int primary key not null auto_increment,
-Codigo_Empresa varchar(100) not null,
-telefono varchar(100) not null,
-Correo varchar(100) not null,
-Estado varchar(100) not null,
-Encargado varchar(100) not null,
-Fecha_inicio datetime not null,
-fecha_fin  datetime not null
+
+CREATE TABLE Empresa (
+    id_Empresa INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nombre_Empresa VARCHAR(100) NOT NULL,
+    codigo_Empresa VARCHAR(100) NOT NULL UNIQUE 
 );
-CREATE TABLE Empresa(
-id_Empresa int primary key  not null auto_increment,
-Nombre_Empresa_Empresa varchar(100) not null
+
+
+CREATE TABLE Trazabilidad (
+    id_Trazabilidad INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    
+    id_Empresa_FK INT NOT NULL, 
+    
+    telefono VARCHAR(100) NOT NULL,
+    Correo VARCHAR(100) NOT NULL,
+    Estado VARCHAR(100)   NOT NULL,
+    Encargado VARCHAR(100) NOT NULL,
+    Fecha_inicio DATETIME NOT NULL,
+    fecha_fin DATETIME NOT NULL,
+
+    CONSTRAINT fk_empresa_trazabilidad 
+    FOREIGN KEY (id_Empresa_FK) REFERENCES Empresa(id_Empresa)
+    ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+INSERT INTO Empresa (nombre_Empresa, codigo_Empresa)
+VALUES ('TICSOCIAL', 'COD1');
+
+INSERT INTO Empresa (nombre_Empresa, codigo_Empresa)
+VALUES ('GENSER POWER COLOMBIA', 'COD2');
+
+INSERT INTO Empresa (nombre_Empresa, codigo_Empresa)
+VALUES ('VC@SOFT S A S', 'COD3');
+
+INSERT INTO Empresa (nombre_Empresa, codigo_Empresa)
+VALUES ('TECPETROL COLOMBIA SAS', 'COD4');
+
+INSERT INTO Empresa (nombre_Empresa, codigo_Empresa)
+VALUES ('INDUSTRIA MILITAR', 'COD5');
+
+INSERT INTO Empresa (nombre_Empresa, codigo_Empresa)
+VALUES 
+('TICSOCIAL', 'COD1'),
+('GENSER POWER COLOMBIA', 'COD2'),
+('VC@SOFT S A S', 'COD3'),
+('TECPETROL COLOMBIA SAS', 'COD4'),
+('INDUSTRIA MILITAR', 'COD5');
+
+
+INSERT INTO Trazabilidad (id_Empresa_FK, telefono, Correo, Estado, Encargado, Fecha_inicio, fecha_fin)
+VALUES (123456, '555-1234', 'contacto@ticsocial.com', 'Activo', 'maria bonilla', '2024-01-01 08:00:00', '2024-12-31 18:00:00');
